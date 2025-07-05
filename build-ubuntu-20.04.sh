4e1b0e63c1507d88845a5a5ba8dee6042e740eca
pocket998 <pocket998@gmail.com>
2025-07-05 11:33:25 +0200

    Update build-ubuntu-20.04.sh

--- build-ubuntu-20.04.sh
+++ build-ubuntu-20.04.sh
@@ -14,6 +14,6 @@ sudo apt-get update; \
   sudo apt-get -y install dotnet-sdk-6.0 git cmake clang ninja-build build-essential libssl-dev pkg-config libboost-all-dev libsodium-dev libzmq5 libgmp-dev libc++-dev zlib1g-dev
 
 (cd src/Miningcore && \
-BUILDIR=${1:-../../build} && \
+BUILDIR=${1:-../../build1} && \
 echo "Building into $BUILDIR" && \
 dotnet publish -c Release --framework net6.0 -o $BUILDIR)
