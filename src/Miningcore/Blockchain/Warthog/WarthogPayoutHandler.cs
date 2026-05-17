89ad0c791982da7fb8e5780637c2480cf618dcb5
pocket998 <pocket998@gmail.com>
2026-05-17 17:01:28 +0200

    Update WarthogPayoutHandler.cs (fichier d'origine)

--- src/Miningcore/Blockchain/Warthog/WarthogPayoutHandler.cs
+++ src/Miningcore/Blockchain/Warthog/WarthogPayoutHandler.cs
@@ -354,10 +354,7 @@ public class WarthogPayoutHandler : PayoutHandlerBase,
                 if(chainInfo?.Error != null)
                     throw new Exception($"'{WarthogCommands.GetChainInfo}': {chainInfo.Error} (Code {chainInfo?.Code})");
 
-                // Force requested fee to 5 WART in E8, but still use Warthog's 16-bit fee encoder.
-                // The from_e8 endpoint expects an integer E8 value, not a decimal WART string.
-                var feeE8Encoded = await restClient.Get<WarthogFeeE8EncodedResponse>(
-                    WarthogCommands.GetFeeE8Encoded.Replace(WarthogCommands.DataLabel, "500000000"), ct);
+                var feeE8Encoded = await restClient.Get<WarthogFeeE8EncodedResponse>(WarthogCommands.GetFeeE8Encoded.Replace(WarthogCommands.DataLabel, maximumTransactionFees.ToString()), ct);
                 if(feeE8Encoded?.Error != null)
                     throw new Exception($"'{WarthogCommands.GetFeeE8Encoded}': {feeE8Encoded.Error} (Code {feeE8Encoded?.Code})");
 
